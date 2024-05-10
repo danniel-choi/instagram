46b7722674b995cfb5186bf4ee8553eadb28f45a
danniel <danniel.choi@siauniverse.com>
2024-05-10 15:10:54 +0900

    npm 업데이트

--- App.js
+++ App.js
@@ -1,17 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { Component } from 'react';
-
-import { View, Text } from 'react-native'
-
-import firebase from 'firebase'
-
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import rootReducer from './redux/reducers'
-import thunk from 'redux-thunk'
-const store = createStore(rootReducer, applyMiddleware(thunk))
-
-
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import firebase from "firebase";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./redux/reducers";
+import thunk from "redux-thunk";
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const firebaseConfig = {
   apiKey: "AIzaSyBK8Cb7Gt9z3zu9z2T-RMX5uCJDfbeNEY8",
@@ -20,35 +15,32 @@ const firebaseConfig = {
   storageBucket: "instagram-dev-b650b.appspot.com",
   messagingSenderId: "621002994424",
   appId: "1:621002994424:web:9516e5e66d536633e9010d",
-  measurementId: "G-TEG50S2281"
+  measurementId: "G-TEG50S2281",
 };
 
-
 if (firebase.apps.length === 0) {
-  firebase.initializeApp(firebaseConfig)
+  firebase.initializeApp(firebaseConfig);
 }
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import LandingScreen from './components/auth/Landing'
-import RegisterScreen from './components/auth/Register'
-import LoginScreen from './components/auth/Login'
-import MainScreen from './components/Main'
-import AddScreen from './components/main/Add'
-import SaveScreen from './components/main/Save'
-import CommentScreen from './components/main/Comment'
-
+import LandingScreen from "./components/auth/Landing";
+import RegisterScreen from "./components/auth/Register";
+import LoginScreen from "./components/auth/Login";
+import MainScreen from "./components/Main";
+import AddScreen from "./components/main/Add";
+import SaveScreen from "./components/main/Save";
+import CommentScreen from "./components/main/Comment";
 
 const Stack = createStackNavigator();
 
-
 export class App extends Component {
   constructor(props) {
-    super()
+    super();
     this.state = {
       loaded: false,
-    }
+    };
   }
 
   componentDidMount() {
@@ -57,30 +49,34 @@ export class App extends Component {
         this.setState({
           loggedIn: false,
           loaded: true,
-        })
+        });
       } else {
         this.setState({
           loggedIn: true,
           loaded: true,
-        })
+        });
       }
-    })
+    });
   }
   render() {
     const { loggedIn, loaded } = this.state;
     if (!loaded) {
       return (
-        <View style={{ flex: 1, justifyContent: 'center' }}>
+        <View style={{ flex: 1, justifyContent: "center" }}>
           <Text>Loading</Text>
         </View>
-      )
+      );
     }
 
     if (!loggedIn) {
       return (
         <NavigationContainer>
           <Stack.Navigator initialRouteName="Landing">
-            <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }} />
+            <Stack.Screen
+              name="Landing"
+              component={LandingScreen}
+              options={{ headerShown: false }}
+            />
             <Stack.Screen name="Register" component={RegisterScreen} />
             <Stack.Screen name="Login" component={LoginScreen} />
           </Stack.Navigator>
@@ -90,17 +86,29 @@ export class App extends Component {
 
     return (
       <Provider store={store}>
-        <NavigationContainer >
+        <NavigationContainer>
           <Stack.Navigator initialRouteName="Main">
             <Stack.Screen name="Main" component={MainScreen} />
-            <Stack.Screen name="Add" component={AddScreen} navigation={this.props.navigation}/>
-            <Stack.Screen name="Save" component={SaveScreen} navigation={this.props.navigation}/>
-            <Stack.Screen name="Comment" component={CommentScreen} navigation={this.props.navigation}/>
+            <Stack.Screen
+              name="Add"
+              component={AddScreen}
+              navigation={this.props.navigation}
+            />
+            <Stack.Screen
+              name="Save"
+              component={SaveScreen}
+              navigation={this.props.navigation}
+            />
+            <Stack.Screen
+              name="Comment"
+              component={CommentScreen}
+              navigation={this.props.navigation}
+            />
           </Stack.Navigator>
         </NavigationContainer>
       </Provider>
-    )
+    );
   }
 }
 
-export default App
+export default App;
